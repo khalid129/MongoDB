7a37036130a3e7d0be5d047d75c78c7e16160ba7
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-06-13 22:31:58 +0500

    insert document using Async wait function

--- NodeJS_conn/src/app.js
+++ NodeJS_conn/src/app.js
@@ -1,2 +1,40 @@
+// Creating connection
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/MERN",{ useNewUrlParser: true , useUnifiedTopology: true } ).then(()=>{console.log("connection is successful")}).catch((err)=>{console.log(err);});
+mongoose.connect("mongodb://localhost:27017/MERN", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection is successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const mernSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  ctype: String,
+  video: Number,
+  active: Boolean,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Collection creation
+const MernStack = new mongoose.model("MernStack",mernSchema);
+
+// create document or insert
+
+const react = new MernStack({
+    name: "React js",
+      ctype: "Front end",
+      video: 80,
+      active: true,
+})
+
+react.save();
