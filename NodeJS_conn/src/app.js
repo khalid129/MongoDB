876e305566bf8d1b1a3bd109e4b4fdf45f1e8d53
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-06-13 20:35:40 +0500

    Creting connection of mongoose with node js

---
+++ NodeJS_conn/src/app.js
@@ -0,0 +1,2 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/MERN",{ useNewUrlParser: true , useUnifiedTopology: true } ).then(()=>{console.log("connection is successful")}).catch((err)=>{console.log(err);});
