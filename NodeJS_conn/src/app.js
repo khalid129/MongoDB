9411f29af81bae3eb0e8a2022e8eb22b0efe25ec
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-06-15 23:10:39 +0500

    insert many document using mongoose

--- NodeJS_conn/src/app.js
+++ NodeJS_conn/src/app.js
@@ -30,11 +30,70 @@ const MernStack = new mongoose.model("MernStack",mernSchema);
 
 // create document or insert
 
-const react = new MernStack({
-    name: "React js",
-      ctype: "Front end",
-      video: 80,
-      active: true,
-})
+// const react = new MernStack({
+//     name: "React js",
+//       ctype: "Front end",
+//       video: 80,
+//       active: true,
+// })
 
-react.save();
+// react.save(); 
+// the above save method use promises so it is better to use async await rather than promise
+
+// create a document using asyn await
+
+// const creatingDocument = async()=>{
+//   try{
+//     const react = new MernStack({
+//       name: "Node js",
+//       ctype : "Back end",
+//       video: 50,
+//       active : true,
+//     })
+    
+//     const result = await react.save();
+//     console.log(result);
+//   }catch(err){
+//     console.log(err);
+//   }
+// }
+
+// creatingDocument();
+
+// Insert Many documents through Mongoose
+
+const creatingDocument = async()=>{
+  try{
+    const htmlWeb = new MernStack({
+      name: "HTML5",
+      ctype : "Front end",
+      video: 50,
+      active : true,
+    })
+    const cssDesign = new MernStack({
+      name: "CSS3",
+      ctype : "Front end",
+      video: 100,
+      active : true,
+    })
+    const javascript = new MernStack({
+      name: "JavaScript",
+      ctype : "Front end",
+      video: 70,
+      active : true,
+    })
+    const angular = new MernStack({
+      name: "Angular js",
+      ctype : "Front end",
+      video: 30,
+      active : true,
+    })
+    
+    const result = await MernStack.insertMany([htmlWeb,cssDesign,javascript,angular]);
+    console.log(result);
+  }catch(err){
+    console.log(err);
+  }
+}
+
+creatingDocument();
