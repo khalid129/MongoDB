12a972092b5a7ed5fb41cb398a67c74201035691
Muhammad Khalid <58352840+khalid129@users.noreply.github.com>
2022-03-07 09:49:11 +0500

    MongoDB work

---
+++ index.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { boolean } = require("webidl-conversions");
+
+mongoose.connect("mongodb://127.0.0.1:27017/sample").then(()=>{
+    console.log("connected to mongoDB successfuly");
+}).catch((err)=>{
+    console.log(err);
+})
+
+const student = new mongoose.Schema({
+    name : String,
+    workout: Boolean,
+    height: Number
+}) ;
+
+const Student = new mongoose.model("Student", student);
+
+const addData = async ()=>{
+    // bad practice
+
+    // const ss = new Student({
+    //     name:"khalid",
+    //     workout:true,
+    //     height:20
+    // })
+
+    // await ss.save();
+
+    // const create = await Student.create({
+    //     name:"uzair",
+    //     workout:true,
+    //     height:20
+    // })
+
+    // const find = await Student.find({height:{$gt:20}})
+    // console.log(find);
+
+    const update = await Student.update({height:20},{$set:{height:10}})
+    // console.log(update)
+
+    console.log(await Student.find())
+}
+
+// addData();
